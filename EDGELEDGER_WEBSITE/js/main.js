7253a88b4520ee2f2faaa95d9766e46f3600b82a
sripad091 <sripad091>
2020-03-26 19:53:29 +0530

    commit

--- EDGELEDGER_WEBSITE/js/main.js
+++ EDGELEDGER_WEBSITE/js/main.js
@@ -14,3 +14,26 @@ center: loc
     //the marker,positined at location
     const marker = new google.maps.Marker({ position:loc,map:map });
 }
+//Stickey menu background
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 150) {
+        this.document.querySelector('#navbar').style.opacity = 0.9;
+    } else {
+        this.document.querySelector('#navbar').style.opacity = 1;
+    }
+});
+//Smooth scrolling
+$('#navbar a, .btn').on('click', function(event) {
+    if (this.hash !=='') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html,body').animate(
+            {
+                scrollTop: $(hash).offset().top - 100
+            },
+            800
+        );
+    }
+});
